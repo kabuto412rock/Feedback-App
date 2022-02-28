7f21e805189a46ab6d621bce1559b66c2d8e97c3
ZongKaKu <kabuto412rock@gmail.com>
2022-02-28 22:15:41 +0800

    完成3.14 State & useState Hook的作法
    只是在FeedbackItem裡面添加useState的用法

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
+import FeedbackItem from "./components/FeedbackItem";
+import Header from "./components/Header";
+
 function App() {
-  const title = "Blog Post";
-  const body = "This is my blog post";
-  const comments = [
-    { id: 1, text: "Comment one" },
-    { id: 2, text: "Comment two" },
-    { id: 3, text: "Comment three" },
-  ];
-
-  const loading = false;
-  const showComments = true;
-
-  if (loading) return <h1>Loading...</h1>;
-
-  const commentBlock = (
-    <div className="comments">
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment) => (
-          <li key={comment.id}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
   return (
-    <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackItem />
+      </div>
+    </>
   );
 }
 
