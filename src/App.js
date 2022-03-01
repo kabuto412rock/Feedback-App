7a7271357b7859c1eabc071c279892e372c98dae
ZongKaKu <kabuto412rock@gmail.com>
2022-03-01 17:09:12 +0800

    [lesson5.26] Updating to the React Router 6
    原本教學是用v5，但如果直接安裝react-router-dom沒有指定版本的話，
    會裝到最新的v6，其中寫法邏輯有點轉換

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import FeedbackItem from "./components/FeedbackItem";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import FeedbackItemList from "./components/FeedbackItemList";
 import Header from "./components/Header";
-
+import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    console.log(newFeedback);
+    setFeedback([newFeedback, ...feedback]);
+  };
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackItem />
+        <Routes>
+          <Route exact path="/" element={
+            <>
+<FeedbackForm handleAdd={addFeedback} />
+            <FeedbackStats feedback={feedback} />
+            <FeedbackItemList
+              feedback={feedback}
+              handleDelete={deleteFeedback}
+            />
+            </>
+          }/>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
